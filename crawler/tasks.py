44a99a0813a21e21016ee480e34b2ca68e6f3cdb
Karol Saputa <karolsaputa@gmail.com>
2020-10-26 20:39:45 +0100

    update scrapy | scrapyscript crawler task | cleanup

--- crawler/tasks.py
+++ crawler/tasks.py
@@ -5,7 +5,8 @@ from datetime import datetime
 import logging
 from configparser import RawConfigParser
 
-from twisted.internet import reactor
+from scrapyscript import Job, Processor
+
 from scrapy.settings import Settings
 from scrapy.crawler import CrawlerProcess
 
@@ -14,12 +15,10 @@ from crawler.ilo.ilo_script import get_lio_data
 from crawler.ilo.ilo_script import extract_html
 from crawler.utils import _short_text_
 
-from crawler.cgrt import CGRT
-from crawler.gov.gov.spiders.gov import GovDuSpider, GovPlCrawler, GovMpSpider
 from crawler.lad.lad.spiders.lad import LadSpider
 from crawler.COVIDPolicyWatch.PolicyWatchSpider import PolicyWatchSpider
 from scheduler.celery import app
-from celery import group
+
 
 from .lad.lad.gov_sites import get_gov_websites
 
@@ -34,64 +33,32 @@ SCRAP_DATE_FORMAT = cfg['elasticsearch']['SCRAP_DATE_FORMAT']
 max_n_chars_to_translate = int(cfg['translator']['max_n_chars_to_translate'])
 
 
-# class CrawlerProcess(Process):
-#     """ This class allows to run scrapy Crawlers using multiprocessing from billiard """
-#
-#     def __init__(self, spider):
-#         Process.__init__(self)
-#         if isinstance(spider, LadSpider):
-#             os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'crawler.lad.lad.settings')
-#         else:
-#             os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'crawler.gov.gov.settings')
-#         settings = get_project_settings()
-#         self.crawler = Crawler(spider.__class__, settings)
-#         self.spider = spider
-#
-#     def run(self):
-#         self.crawler.crawl(self.spider)
-#         reactor.run()
+@app.task(queue='crawler')
+def crawl_lad_scrapyscript(depth=5, urls=None, domain='gov'):
+    settings = Settings()
+    settings.set('MEDIA_ALLOW_REDIRECTS', True)
+    settings.set('SCHEDULER_PRIORITY_QUEUE', 'scrapy.pqueues.DownloaderAwarePriorityQueue')
+    settings.set('COOKIES_ENABLED', False)
+    settings.set('DEFAULT_REQUEST_HEADERS', {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'pl-PL,pl;q=0.9,en-GB;q=0.8,en;q=0.7,en-US;q=0.6',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'dnt': '1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    })
+    settings.set('CONCURRENT_REQUESTS', 64)
+    settings.set('DEPTH_LIMIT', depth)
 
+    if urls is None:
+        urls = list(get_gov_websites(gov_sites_path))
 
-# @app.task
-# def crawl_gov_du():
-#     """ Starts crawling process which downloads pdfs from dziennikustaw.gov.pl """
-#     logging.info("crawl gov du")
-#     spider = GovDuSpider()
-#     process = CrawlerProcess(spider)
-#     process.start()
-#     process.join()
-
-
-# @app.task
-# def crawl_gov_mp():
-#     """ Starts crawling process which downloads pdfs from monitorpolski.gov.pl """
-#     spider = GovMpSpider()
-#     process = CrawlerProcess(spider)
-#     process.start()
-#     process.join()
-
-
-# @app.task
-# def run_crawl_gov_du():
-#     run_task = crawl_gov_du.s()
-#     run_task.link(update_crawling_date.s('GovDuSpider'))
-#     run_task.delay()
-
-
-# @app.task
-# def run_crawl_gov_mp():
-#     run_task = crawl_gov_mp.s()
-#     run_task.link(update_crawling_date.s('GovMpSpider'))
-#     run_task.delay()
-
-
-# @app.task
-# def crawl_gov():
-#     """Starts crawling process which downloads pdfs from all websites in domain gov.pl"""
-#     crawler = GovPlCrawler()
-#     process = CrawlerProcess(crawler)
-#     process.start()
-#     process.join()
+    job = Job(LadSpider, urls, domain)
+    processor = Processor(settings=settings)
+    data = processor.run([job])
+    print(json.dumps(data, indent=4))
 
 
 @app.task(queue='crawler')
