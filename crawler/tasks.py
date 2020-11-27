2a7719b506dd0847cab3165d806ef708148f84d6
Karol Saputa <karolsaputa@gmail.com>
2020-11-28 00:43:29 +0100

    requests delay setting

--- crawler/tasks.py
+++ crawler/tasks.py
@@ -45,7 +45,8 @@ lad_crawler_settings = {
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
         },
-    'DOWNLOAD_TIMEOUT': 20
+    'DOWNLOAD_TIMEOUT': 20,
+    'DOWNLOAD_DELAY': 0.05
 }
 
 
